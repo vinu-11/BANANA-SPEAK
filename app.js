a572afae1831c59e21b4e9183b5904a76185f3a4
vinu-11 <hundekarvinay@gmail.com>
2023-03-08 18:58:21 +0530

    Added API and made changes

--- app.js
+++ app.js
@@ -2,9 +2,32 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-area");
 var outputDiv = document.querySelector("#output");
 
-function clickEventHandler(){
-    outputDiv.innerHTML = "jnjknjnxd" + txtInput.value;
+//var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
+
+function getTranslationURL(text){
+    return serverURL + "?" + "text=" + text
+
 }
+
+function errorHandler(error){
+    console.log("Error occured",error);
+    alert("Something wrong with server! Try again after some time")
+}
+
+function clickEventHandler(){
+   // outputDiv.innerHTML = "jnjknjnxd" + txtInput.value;
+   var inputTxt = txtInput.value;
+
+    fetch(getTranslationURL(inputTxt))
+    .then(response => response.json())
+    .then(json => {
+        var transaltedTxt = json.contents.translated;
+        outputDiv.innerText = transaltedTxt;
+        })
+    .catch(errorHandler)
+};
 btnTranslate.addEventListener("click", clickEventHandler);
 
 
